8943bdf10bd490502c5088257394162659462527
Zachary Knott <zmk5@duke.edu>
2021-02-01 13:26:42 -0500

    added bb

--- main.py
+++ main.py
@@ -13,24 +13,30 @@ archive1 = 0
 archive2 = 0
 
 while(True):
+    
     url_1 = requests.get('https://finance.yahoo.com/quote/GME')
     url_2 = requests.get('https://finance.yahoo.com/quote/AMC')
+    url_3 = requests.get('https://finance.yahoo.com/quote/BB')
 
     gme = (make_soup(url_1))
     amc = (make_soup(url_2))
+    bb =  (make_soup(url_3))
 
     print1 = "GME price: " + gme
     print2 = "AMC price: " + amc
+    print3 = "BB price: " + bb
     
     
-    #os.system('cls' if os.name == 'nt' else 'clear')
+    os.system('cls' if os.name == 'nt' else 'clear')
+
+    print("==============================")
     print(print1)
     print(print2)
+    print(print3)
     print("Time: " + t.asctime())
-    print("=================================")
-    archive1 = gme
-    archive2 = amc
-    t.sleep(3)
+    print("==============================")
+
+    t.sleep(1)
     
 
 
