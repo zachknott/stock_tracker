1ac27569c942cb850314da890992eea62134bc0e
Zachary Knott <zmk5@duke.edu>
2021-02-01 14:14:08 -0500

    Refresh time constant added, and setup for even refresh times now

--- main.py
+++ main.py
@@ -11,37 +11,37 @@ url_list = ['https://finance.yahoo.com/quote/GME',
 
 name_list = ['GME','AMC','BB_','NOK']
 
-NUM_STONKS = 4
+# How many STONKS do you want to iterate through
+NUM_STONKS = 2
+REFRESH_TIME_SECONDS = 5
 
 def make_soup(name):
     soup = bs4.BeautifulSoup(name.text, features="html.parser")
     price = soup.find_all("div", {'class': 'D(ib) Mend(20px)'})[0].find('span').text
     return price
 
-archive1 = 0
-archive2 = 0
-
 while(True):
+    time_before = t.localtime().tm_sec
+
     requests_list = []
-    
-    for url in url_list:
-        requests_list.append(requests.get(url))
-    
     price_list = []
 
-    for request in requests_list:
-        price_list.append(make_soup(request))
+    for x in range(NUM_STONKS):
+        requests_list.append(requests.get(url_list[x]))
+        price_list.append(make_soup(requests_list[x]))
 
     os.system('cls' if os.name == 'nt' else 'clear')
-
     print("==============================")
+
     for x in range(NUM_STONKS):
         print(name_list[x] + " price: " + price_list[x])
     
     print("Time: " + t.asctime())
     print("==============================")
 
-    t.sleep(1)
+    time_after = t.localtime().tm_sec
+
+    t.sleep(REFRESH_TIME_SECONDS -( time_after - time_before))
     
 
 
