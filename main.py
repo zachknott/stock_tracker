8c5c762867608b3e5a0a7c017ce34e55084114e6
Zachary Knott <zmk5@duke.edu>
2021-02-01 20:07:40 -0500

    After hours has an index bug sometimes, also prints message when all trading is done

--- main.py
+++ main.py
@@ -6,23 +6,21 @@ from bs4 import BeautifulSoup
 from requests_own import request_head
 from requests_own import make_url
 
+#All MODIFIABLE VARIABLES#
 
+# Add Symbols to the stock tracker
+SYMBOL_LIST = ['GME','AMC','BB','NOK','VTI',"VOO"]
+# How many symbols do you want to iterate through
+NUM_STONKS = 2
+# what refresh time in seconds
+REFRESH_TIME_SECONDS = 5
 
+# Warning Beep
+STOCK_NUM = 0
+MAX_PRICE = 200
+MIN_PRICE = 150
 
 if __name__ == "__main__":
-
-    # Add Symbols to the stock tracker
-    SYMBOL_LIST = ['GME','AMC','BB','NOK','VTI',"VOO"]
-    # How many symbols do you want to iterate through
-    NUM_STONKS = 2
-    # what refresh time in seconds
-    REFRESH_TIME_SECONDS = 5
-
-    # Warning
-    STOCK_NUM = 0
-    MAX_PRICE = 318
-    MIN_PRICE = 200
-
     # Setup runs once
     url_list = make_url(NUM_STONKS,SYMBOL_LIST)
 
@@ -39,17 +37,19 @@ if __name__ == "__main__":
         for x in range(NUM_STONKS):
             price_list.append(request_head(requests_list[x]))
             
-        t.sleep(3)
         os.system('cls' if os.name == 'nt' else 'clear')            # Clears the terminal
         print("==============================")
 
         for x in range(NUM_STONKS):
             print(SYMBOL_LIST[x] + " price: " + price_list[x])
 
-            if float(price_list[STOCK_NUM]) > MAX_PRICE or float(price_list[STOCK_NUM]) < MIN_PRICE:
+            if x == STOCK_NUM and (float(price_list[STOCK_NUM]) > MAX_PRICE or float(price_list[STOCK_NUM]) < MIN_PRICE):
                 print("\a")
     
         print("Time: " + t.asctime())
+
+        if t.localtime().tm_hour >= 20 or t.localtime().tm_hour < 4:
+            print("\nTRADING CLOSED FOR THE DAY\n")
         print("==============================")
 
         time_after = t.localtime().tm_sec
